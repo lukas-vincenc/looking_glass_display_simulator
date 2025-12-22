fa2ae9c557bb44638df0bf090f99c34071420442
lukas-vincenc <lucky.vincenc@gmail.com>
2025-12-22 16:43:02 +0100

    added properties to the lens vis panel

--- lens_visualization/lens_visualization_panel.py
+++ lens_visualization/lens_visualization_panel.py
@@ -1,6 +1,40 @@
 import bpy
+from bpy.props import FloatProperty
 
 from .scene_spawner import SceneSpawner
+
+
+def update_ray_source_x(self, context):
+    obj = bpy.data.objects.get("Ray_Source")
+    if obj is None:
+        return
+
+    obj.location.x = self.ray_source_x / 10
+
+
+def update_ray_source_rotation(self, context):
+    obj = bpy.data.objects.get("Ray_Source")
+    if obj is None:
+        return
+
+    obj.rotation_euler.z = self.ray_source_rotation
+
+
+class CustomProps(bpy.types.PropertyGroup):
+    ray_source_x: FloatProperty(
+        name="Ray Source X",
+        default=0.0,
+        subtype='DISTANCE',
+        update=update_ray_source_x
+    )
+
+    ray_source_rotation: FloatProperty(
+        name="Ray Source Rotation",
+        default=0.0,
+        subtype='ANGLE',
+        unit='ROTATION',
+        update=update_ray_source_rotation
+    )
 
 
 class LensVisualizationPanel(bpy.types.Panel):
@@ -12,10 +46,16 @@ class LensVisualizationPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+        cus_pt = context.scene.lvis_custom_props
+
+        layout.prop(cus_pt, "ray_source_x")
+        layout.prop(cus_pt, "ray_source_rotation")
+
         layout.operator("object.scene_spawner", text="Spawn Scene")
 
 
 all_classes = [
+    CustomProps,
     LensVisualizationPanel,
     SceneSpawner
 ]
@@ -24,8 +64,10 @@ all_classes = [
 def register():
     for cls in all_classes:
         bpy.utils.register_class(cls)
+    bpy.types.Scene.lvis_custom_props = bpy.props.PointerProperty(type=CustomProps)
 
 
 def unregister():
     for cls in all_classes:
         bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.lvis_custom_props
